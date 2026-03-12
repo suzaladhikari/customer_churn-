2330b670e72a88af1093f09d2c7ea0c914ecdb06
Peter <adhikarisuzal944@gmail.com>
2026-03-11 21:51:22 -0400

    created the pydantic model for the input data

--- fastapi/fastapi.py
+++ fastapi/fastapi.py
@@ -2,3 +2,32 @@ import joblib
 from fastapi import FastAPI 
 from pydantic import BaseModel
 from fastapi import HTTPException
+
+class Data(BaseModel):
+    gender:int
+    SeniorCitizen:int 
+    Partner:int 
+    Dependents:int
+    tenure:int= 0
+    PhoneService:int = 0
+    MultipleLines:int = 0
+    InternetService:int = 0 
+    OnlineSecurity:int = 0
+    OnlineBackup:int = 0 
+    DeviceProtection:int = 0 
+    TechSupport:int = 0 
+    StreamingTV:int = 0 
+    StreamingMovies:int = 0 
+    Contract:int
+    PaperlessBilling:int = 0 
+    PaymentMethod:int
+    MonthlyCharrges: int
+    TotalCharges:int 
+
+
+
+# ###gender', 'SeniorCitizen', 'Partner', 'Dependents', 'tenure',
+#        'PhoneService', 'MultipleLines', 'InternetService', 'OnlineSecurity',
+#        'OnlineBackup', 'DeviceProtection', 'TechSupport', 'StreamingTV',
+#        'StreamingMovies', 'Contract', 'PaperlessBilling', 'PaymentMethod',
+#        'MonthlyCharges', 'TotalCharges', 'Churn'
