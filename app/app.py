562a4200aadae5a4fd544fd71b595abf07563a1d
Peter <adhikarisuzal944@gmail.com>
2026-03-12 20:08:44 -0400

    created the header why is customer churn important

--- app/app.py
+++ app/app.py
@@ -2,3 +2,10 @@ import streamlit as st
 st.title("Churn Detection Model")
 st.header("What is Churn Detection?")
 st.subheader("Churn detection is the process of identifying customers likely to stop using a product or service, often using machine learning to analyze behavior and predict risks. Once you can identify those customers that are at risk of cancelling, you should know exactly what marketing action to take for each individual customer to maximise the chances that the customer will remain.")
+
+st.divider()
+
+st.header("Why is the Customer Churn Important ")
+st.subheader("Customer churn is a common problem across businesses in many sectors. If you want to grow as a company, you have to invest in acquiring new clients. Every time a client leaves, it represents a significant investment lost. Both time and effort need to be channelled into replacing them. Being able to predict when a client is likely to leave, and offer them incentives to stay, can offer huge savings to a business.")
+
+
