658254a638d5a39bc8b7e07175d0af6fd03390d5
Peter <adhikarisuzal944@gmail.com>
2026-03-12 20:04:35 -0400

    done with the header of what is churn detection

--- app/app.py
+++ app/app.py
@@ -1,2 +1,4 @@
 import streamlit as st
-st.header("Welcome to the Churn Detection Model")
+st.title("Churn Detection Model")
+st.header("What is Churn Detection?")
+st.subheader("Churn detection is the process of identifying customers likely to stop using a product or service, often using machine learning to analyze behavior and predict risks. Once you can identify those customers that are at risk of cancelling, you should know exactly what marketing action to take for each individual customer to maximise the chances that the customer will remain.")
